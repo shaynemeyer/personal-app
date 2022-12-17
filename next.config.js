3cac15e4605a6036df96b4396e7d4d29338bce0a
Shayne Meyer <shayne.meyer@gmail.com>
2022-12-17 09:14:55 -0800

    finished initial app structure

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      'tailwindui.com',
+      'images.unsplash.com',
+      'eincode.com',
+      'thrangra.sirv.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
